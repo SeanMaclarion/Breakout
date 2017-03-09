e32c7b51cc1b1aa86b12005353a4b4f6c3b4303d
Sean MacLarion <maclaris1@hawkmail.newpaltz.edu>
2017-03-09 10:14:41 -0500

    Added health to bricks

--- Assets/Brick.cs
+++ Assets/Brick.cs
@@ -2,8 +2,13 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
 public class Brick : MonoBehaviour
 {
+    public int health = 1;
     // Use this for initialization
     void Start()
     {
@@ -12,6 +17,10 @@ public class Brick : MonoBehaviour
 
     void OnCollisionEnter2D(Collision2D coll)
     {
-        gameObject.SetActive(false);
+        health--;
+        if (health == 0)
+        {
+            gameObject.SetActive(false);
+        }
     }
 }
