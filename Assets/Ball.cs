8253f23494f01c0d5691e4dea1ab9e721a4d4b7c
Sean MacLarion <maclaris1@hawkmail.newpaltz.edu>
2017-03-13 20:13:43 -0400

    Added New Scenes for Midterm

--- Assets/Ball.cs
+++ Assets/Ball.cs
@@ -1,24 +1,31 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class Ball : MonoBehaviour {
+public class Ball : MonoBehaviour
+{
 
     public Vector2 startingVelocity = new Vector2(15, -20);
     private Vector3 startingPosition;
     public GameObject gameOverSign;
     public GameObject youWinSign;
+    public Text livesValue;
+    public Text pointsValue;
 
     int lives = 3;
+    int points = 0;
 
-	// Use this for initialization
-	void Start ()
+    // Use this for initialization
+    void Start()
     {
+        livesValue.text = lives.ToString();
+
         GetComponent<Rigidbody2D>().velocity = startingVelocity;
         startingPosition = transform.position;
-	}
-	// Update is called once per frame
-	void Update ()
+    }
+    // Update is called once per frame
+    void Update()
     {
         if (transform.position.y < -0.25f)
         {
@@ -35,6 +42,7 @@ public class Ball : MonoBehaviour {
     {
         Debug.Log("You are out");
         lives--;
+        livesValue.text = lives.ToString();
         transform.position = startingPosition;
         GetComponent<Rigidbody2D>().velocity = new Vector2();
 
@@ -51,10 +59,29 @@ public class Ball : MonoBehaviour {
 
     public void YouBrokeABrick()
     {
+        points++;
+        pointsValue.text = points.ToString();
         var bricksLeft = FindObjectsOfType<Brick>().Length;
-        if(bricksLeft == 0)
+        if (bricksLeft == 0)
         {
             youWinSign.SetActive(true);
         }
+
+    }
+
+    public void Teleport()
+    {
+        transform.position = startingPosition;
+
+    }
+    public void SpeedUp()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, -30);
+
+
+    }
+    public void SlowDown()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(0, -2);
     }
 }
--- Assets/Brick.cs
+++ Assets/Brick.cs
@@ -9,6 +9,9 @@ using UnityEngine;
 public class Brick : MonoBehaviour
 {
     public int health = 1;
+    public bool isTeleport = false;
+    public bool isSpeed = false;
+    public bool isSlow = false;
     // Use this for initialization
     void Start()
     {
@@ -18,10 +21,23 @@ public class Brick : MonoBehaviour
     void OnCollisionEnter2D(Collision2D coll)
     {
         health--;
+        if (isTeleport == true)
+        {
+            FindObjectOfType<Ball>().Teleport();
+        }
+        if (isSpeed == true)
+        {
+            FindObjectOfType<Ball>().SpeedUp();
+        }
+        if (isSlow == true)
+        {
+            FindObjectOfType<Ball>().SlowDown();
+        }
         if (health == 0)
         {
             gameObject.SetActive(false);
             FindObjectOfType<Ball>().YouBrokeABrick();
         }
+      
     }
 }
